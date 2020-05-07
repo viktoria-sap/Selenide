1da12adc0ee5b4f1c95c17eadeb55034c9b3ea62
vika <v.sapunkova@neurobotics.ru>
2020-05-07 17:34:02 +0300

    changed data

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,6 +1,8 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -13,9 +15,11 @@ class RegistrationTest {
     void shouldRegisterForDelivery() {
         Calendar date = new GregorianCalendar();
         date.add(Calendar.DAY_OF_YEAR, 5);
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
+        String str = format1.format(date.getTime());
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Волгоград");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(date));
+        $("[placeholder='Дата встречи']").setValue(str);
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79680000000");
         $("span[class='checkbox__text']").click();
