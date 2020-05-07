d04d741b5b00a747bfccb0497126a4f1b9522a23
vika <v.sapunkova@neurobotics.ru>
2020-05-07 16:17:08 +0300

    problems with data

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -2,6 +2,9 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -9,9 +12,11 @@ import static com.codeborne.selenide.Selenide.*;
 class RegistrationTest {
     @Test
     void shouldRegisterForDelivery() {
+        Calendar date = new GregorianCalendar();
+        date.add(Calendar.DAY_OF_YEAR, 5);
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Волгоград");
-        $("[placeholder='Дата встречи']").setValue("11.05.2020");
+        $("[placeholder='Дата встречи']").setValue(String.valueOf(date));
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79680000000");
         $("span[class='checkbox__text']").click();
